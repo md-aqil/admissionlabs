7004e4fcdcc8507ee2b8862baecc189bab61d21d
Md Aqil <aqilali381@gmail.com>
2020-03-21 23:51:24 +0530

    added

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,36 +1,40 @@
+<header class="header sticky-header" data-scroll>
+				<nav class="navbar navbar-expand-lg bg-transparent">
 
+					<div class="c-container f-center">
+					<a class="navbar-brand" href="#">
+						<img class="site-logo" src="img/logo.png" alt="">
+					</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					  <span class="navbar-toggler-icon"></span>
+					</button>
+				  
+					<div class="collapse navbar-collapse nav-items" id="navbarSupportedContent">
+					  <ul class="navbar-nav mr-auto main-nav">
+						<li class="nav-item active">
+							<a class="nav-link active" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+							<a class="nav-link" href="#">Councellor</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">SCHOLARSHIPS</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">DREAM INSTITUTION</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">BLOGS</a>
+								</li>
+					  </ul>
+					  <form class="form-inline">
+						<button class="btn btn-outline-white mr-2" type="submit">LIST YOURSELF</button>
+							<button class="btn btn-outline-white mr-2" type="submit">REGISTER</button>
+							<button class="btn btn-primary" data-toggle="modal" data-target="#one">LOGIN</button>
+					  </form>
+					</div>
 
-	<header class="header sticky-header" data-scroll>
-		<div class="c-container">
-			<div class="nav-items">
-			<a href="">
-			<img class="site-logo" src="img/logo.png" alt="">
-			</a>
-			<ul class="main-nav">
-				<li class="nav-item active">
-				<a class="nav-link active" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-				<a class="nav-link" href="#">Counceller</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">SCHOLARSHIPS</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">DREAM INSTITUTION</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">BLOGS</a>
-					</li>
-			</ul>
-			
-			<div class="form-inline my-2 my-lg-0">
-								<button class="btn btn-outline-white mr-2" type="submit">LIST YOURSELF</button>
-								<button class="btn btn-outline-white mr-2" type="submit">REGISTER</button>
-								<button class="btn btn-primary" data-toggle="modal" data-target="#one">LOGIN</button>
-							</div>
-			</div>
-		</div>
-		
-	</header> 
-	
+				</div>
+				  </nav>
+			</header> 
+
